c226fea05fabffd315d46ae7e546f994a8ee389a
sanjaraT <sanjaratunola@gmail.com>
2026-08-26 18:27:16 +0600

    stage 1 : root and health endpoints

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def hello():
-    return {"message": "Hello!"}
+def root():
+    return {
+        "name":"Task API",
+        "version":"1.0",
+        "endpoints":["/tasks"]
+    }
+@app.get("/health")
+def health():
+    return {"status":"OK"}
