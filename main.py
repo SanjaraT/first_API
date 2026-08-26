d988a9d1929deda21346edb18f17a354cecc5eb7
sanjaraT <sanjaratunola@gmail.com>
2026-08-26 19:59:18 +0600

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
@@ -12,6 +13,12 @@ tasks = [
 # pydantic schema for creating task
 class CreateTask(BaseModel):
     title : str
+
+# shema for update
+class UpdateTask(BaseModel):
+    title : Optional[str] = None
+    done : Optional[bool] = None
+
 
 # status
 @app.get("/")
@@ -51,3 +58,26 @@ def create_task(new_task: CreateTask):
     task = {"id": next_id, "title": new_task.title, "done": False}
     tasks.append(task)
     return task
+
+# Update / Edit task
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, update : UpdateTask):
+    for task in tasks:
+        if task["id"] == task_id:
+            if update.title is not None:
+                if not update.title.strip():
+                    raise HTTPException(status_code=400, detail = "Title cannot be empty")
+                task["title"] = update.title
+            if update.done is not None:
+                task["done"] = update.done
+            return task
+    raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
+
+
+# delete  task
+@app.delete("/tasks/{task_id}", status_code=204)
+def delete_task(task_id : int):
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return 
